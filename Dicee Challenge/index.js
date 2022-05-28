5bb45fa3909e0fb580ae3dfa6afbc3c34bc4921e
avneet singh <96073704+avneet-singh843@users.noreply.github.com>
2022-05-28 21:15:19 +0530

    Add files via upload

---
+++ Dicee Challenge/index.js
@@ -0,0 +1,23 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png"
+var randomImageSource = "images/" + randomDiceImage;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber2 + ".png"
+var randomImageSource = "images/" + randomDiceImage;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource);
+
+var winner = document.querySelector("h1");
+
+if(randomNumber1>randomNumber2){
+  winner.innerHTML = "🚩Player 1 wins!";
+}
+else if (randomNumber1<randomNumber2) {
+  winner.innerHTML = "Player 2 wins!🚩";
+}
+else{
+  winner.innerHTML = "Draw!!"
+}
